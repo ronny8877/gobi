07159218831dd77ce68e927925ad0df84ca129aa
rny <sunnyrawat225@gmail.com>
2024-09-12 03:57:31 +0530

    inital docs setup

---
+++ docs/astro.config.mjs
@@ -0,0 +1,11 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import mdx from '@astrojs/mdx';
+
+import tailwind from '@astrojs/tailwind';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [mdx(), tailwind()]
+});
